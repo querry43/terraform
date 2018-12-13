a597e09a514b1f44193abed8a98d725ccddbf7d3
Matt Harrington <querry43@gmail.com>
2018-12-13 16:58:38 +0000

    adding ssl certs

--- modules/aws_s3_bucket_site/distribution.tf
+++ modules/aws_s3_bucket_site/distribution.tf
@@ -36,8 +36,7 @@ resource aws_cloudfront_distribution distribution {
   price_class = var.price_class
 
   viewer_certificate {
-    cloudfront_default_certificate = true
-    # acm_certificate_arn = aws_acm_certificate.cert.arn
-    # ssl_support_method  = "sni-only"
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
   }
 }
